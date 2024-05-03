7d057169bd05a962848f294fa41df29619089f2e
rslopes9 <rs.lopes@campus.fct.unl.pt>
2024-05-03 13:12:53 +0100

    Pushed the project to git

---
+++ sd2324-tp1/src/tukano/impl/rest/servers/RestBlobsResource.java
@@ -0,0 +1,28 @@
+package tukano.impl.rest.servers;
+
+import tukano.impl.api.java.ExtendedBlobs;
+import tukano.impl.api.rest.RestExtendedBlobs;
+import tukano.impl.java.servers.JavaBlobs;
+
+public class RestBlobsResource extends RestResource implements RestExtendedBlobs {
+
+	final ExtendedBlobs impl;
+	public RestBlobsResource() {
+		this.impl = new JavaBlobs();
+	}
+	
+	@Override
+	public void upload(String blobId, byte[] bytes) {
+		super.resultOrThrow( impl.upload(blobId, bytes));
+	}
+
+	@Override
+	public byte[] download(String blobId) {
+		return super.resultOrThrow( impl.download( blobId ));
+	}
+
+	@Override
+	public void deleteAllBlobs(String userId, String password) {
+		super.resultOrThrow( impl.deleteAllBlobs( userId, password ));
+	}
+}
